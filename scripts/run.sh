7635a06eef5dd2d0fb1359fe0c67b5b4c0bc9a78
Peter Woo <hw527@cam.ac.uk>
2023-10-13 05:39:14 +0100

    feat: create launch scripts and set ip addresses automatically given robot id

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-nvidia-docker run --detach \
+# Set your image name and container name
+IMAGE_NAME="orin_drones:latest"
+CONTAINER_NAME="orin_drones"
+
+# Function to start an existing container and execute a bash interface
+start_existing_container() {
+  if [[ "$(docker ps -a --filter "name=$CONTAINER_NAME" --format "{{.ID}}")" ]]; then
+    echo "Starting existing container $CONTAINER_NAME..."
+    nvidia-docker start $CONTAINER_NAME
+    nvidia-docker exec -it $CONTAINER_NAME bash
+  else
+    echo "No existing container found with name $CONTAINER_NAME."
+  fi
+}
+
+# Check if the container exists and start or create it
+if [[ "$(docker ps -a --filter "name=$CONTAINER_NAME" --format "{{.ID}}")" ]]; then
+  start_existing_container
+else
+  nvidia-docker run --detach \
     --gpus all \
     --privileged \
     --net=host \
     --device=/dev/ttyTHS1 \
     --volume ${PWD}:/home/:Z \
-    -it orin_drones_jammy:latest "$@"
+    --name $CONTAINER_NAME \
+    -it $IMAGE_NAME
+fi
--- scripts/setup_drones.sh
+++ scripts/setup_drones.sh
@@ -1,4 +1,34 @@
-#!/bin/sh
+#!/usr/bin/bash
+
+# Check if an integer argument was provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <integer>"
+    exit 1
+fi
+
+# Check if the argument is a valid integer
+if ! [[ $1 =~ ^[0-9]+$ ]]; then
+    echo "Error: Argument is not an integer."
+    exit 1
+fi
+
+robot_id=$1
+echo "Setting up ip and directories for Robot $robot_id"
+
+static_ip="{STATIC_IP}$robot_id/24"
+
+sudo nmcli con add \
+     con-name "{CONNECTION_NAME}" \
+     type wifi \
+     ifname wlan0 \
+     ssid "{SSID}" \
+     -- wifi-sec.key-mgmt wpa-psk \
+     wifi-sec.psk "{PASSWORD}" \
+     ipv4.method manual \
+     ipv4.gateway "{GATEWAY}" \
+     ipv4.address $static_ip \
+     ipv4.dns "{NAMESERVER}" \
+     ipv6.method disabled
 
 cd /home/ubuntu/
 mkdir -p ros2_drone_ws/src
@@ -8,3 +38,11 @@ cp /home/ubuntu/orin-bringup/scripts/run.sh /home/ubuntu/ros2_drone_ws/
 
 cd /home/ubuntu/orin-bringup
 nvidia-docker build -t orin_drones:latest .
+
+cd /home/ubuntu/orin-bringup/scripts/drone_configfiles/
+sed -i 's/value: "U[0-9]*"/value: "U'"$robot_id"'"/' indoor_flight.launch.yaml
+sed -i "s/{ *name: *\"numeric_id\", *value: *\"[0-9]*\" *}/{ name:   \"numeric_id\",           value: \"$robot_id\" }/" indoor_flight.launch.yaml
+
+cp freyja_flight.launch.yaml /home/ubuntu/ros2_drone_ws/src/Freyja/
+cp mavros_config_freyja.yaml /home/ubuntu/ros2_drone_ws/src/Freyja/freyja_configfiles/launch/
+cp indoor_flight.launch.yaml /home/ubuntu/ros2_drone_ws/
